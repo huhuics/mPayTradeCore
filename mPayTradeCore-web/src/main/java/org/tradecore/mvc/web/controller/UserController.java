3b29be1c2ab55b3d13354077d9a2ce79fdcd944c
huhuics <hhuics@163.com>
2016-07-03 20:45:15 +0800

    mPayTradeCore跑通

--- mPayTradeCore-web/src/main/java/org/tradecore/mvc/web/controller/UserController.java
+++ mPayTradeCore-web/src/main/java/org/tradecore/mvc/web/controller/UserController.java
@@ -5,14 +5,9 @@
 package org.tradecore.mvc.web.controller;
 
 import javax.annotation.Resource;
-import javax.jms.JMSException;
-import javax.jms.Message;
-import javax.jms.Session;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.jms.core.JmsTemplate;
-import org.springframework.jms.core.MessageCreator;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -36,9 +31,6 @@ public class UserController {
     @Resource
     private UserFacadeClient    userFacadeClient;
 
-    @Resource
-    private JmsTemplate         jmsTemplate;
-
     @RequestMapping(value = "/showUser", method = RequestMethod.GET)
     public String toShowUser(WebRequest request, ModelMap map) {
         try {
@@ -52,25 +44,4 @@ public class UserController {
         return "showUser";
     }
 
-    @RequestMapping(value = "/sendMsg", method = RequestMethod.GET)
-    public String toSendMsg(WebRequest request, ModelMap map) {
-        try {
-            Result<UserDTO> userRet = userFacadeClient.selectUserById(1L);
-            final UserDTO userDTO = userRet.getResultObj();
-            map.put("user", userDTO);
-
-            //向mPayTradeCore-biz-impl发送消息
-            logger.info("开始发送消息,内容:{}", userDTO);
-            jmsTemplate.send(new MessageCreator() {
-                @Override
-                public Message createMessage(Session session) throws JMSException {
-                    return session.createObjectMessage(userDTO);
-                }
-            });
-        } catch (Exception e) {
-            logger.error("查询用户信息异常", e);
-        }
-        logger.info("消息发送完毕");
-        return "sendMsg";
-    }
 }
