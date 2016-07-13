f4ce50bfa3fb6176d3643c6590c6c76ccce569a4
huhuics <hhuics@163.com>
2016-07-13 11:00:45 +0800

    增加异步通知Controller

---
+++ mPayTradeCore-web/src/main/java/org/tradecore/mvc/web/controller/TradeNotifyController.java
@@ -0,0 +1,39 @@
+/**
+ * Beijing Jiaotong University
+ * Copyright (c) 1896-2016 All Rights Reserved.
+ */
+package org.tradecore.mvc.web.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.context.request.WebRequest;
+import org.tradecore.common.util.LogUtil;
+
+/**
+ * 接收支付宝扫码支付异步通知
+ * @author HuHui
+ * @version $Id: TradeNotifyController.java, v 0.1 2016年7月13日 上午10:52:46 HuHui Exp $
+ */
+@Controller
+@RequestMapping("/tradeNotify")
+public class TradeNotifyController {
+
+    private static final Logger logger  = LoggerFactory.getLogger(TradeNotifyController.class);
+
+    private static final String SUCCESS = "success";
+
+    private static final String RESULT  = "result";
+
+    @RequestMapping(value = "/receive", method = RequestMethod.POST)
+    public void receive(WebRequest request, ModelMap map) {
+
+        LogUtil.info(logger, "收到支付宝扫码支付异步通知");
+
+        map.put(RESULT, SUCCESS);
+    }
+
+}
