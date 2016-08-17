54f63cc1d431d09ab0bae2f48c8810ce46234489
prd-wfc <prd-wfc@WFC-PC.fesc.com>
2016-08-17 21:10:19 +0800

    commit 加入收单机构pay测试

---
+++ mPayTradeCore-service/src/test/java/org/tradecore/acquirertest/AcquirerPayRequest.java
@@ -0,0 +1,336 @@
+package org.tradecore.acquirertest;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+
+import org.apache.commons.httpclient.NameValuePair;
+import org.apache.commons.lang3.StringUtils;
+import org.tradecore.common.util.AssertUtil;
+import org.tradecore.common.util.DateUtil;
+
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.annotation.JSONField;
+
+/**
+ * 
+ * @author prd-wfc
+ *
+ */
+public class AcquirerPayRequest{
+    
+    public BizRequest bizrequest = new BizRequest();
+    
+    /** (必填)收单机构编号 */
+    @JSONField(name = "acquirer_id")
+    protected String            acquirerId;
+
+    /** (必填)支付宝分配给开发者的应用ID */
+    @JSONField(name = "app_id")
+    protected String            appId;
+    
+    /** (必填)接口名称 */
+    @JSONField(name = "method")
+    protected String            method;
+
+    /** (必填)仅支持JSON */
+    @JSONField(name = "format")
+    protected String            format;
+    
+    /** (必填)请求使用的编码格式，utf-8*/
+    @JSONField(name = "charset")
+    protected String            charset;
+
+    /** (必填)商户生成签名字符串所使用的签名算法类型*/
+    @JSONField(name = "sign_type")
+    protected String            signType;
+    
+    /** (必填)商户请求参数的签名串 */
+    @JSONField(name = "sign")
+    protected String            sign;
+
+    /** (必填)发送请求的时间 */
+    @JSONField(name = "timestamp")
+    protected String            timestamp;
+    
+    /** (必填)调用接口版本号 */
+    @JSONField(name = "version")
+    protected String            version;
+    
+    /** (必填)结算中心服务器主动通知收单机构服务器里指定页面http路径 */
+    @JSONField(name = "notify_url")
+    protected String            notifyUrl;
+    
+    /** (必填)应用授权令牌 */
+    @JSONField(name = "app_auth_token")
+    protected String            appAuthToken;
+    
+    /** (必填)付款渠道 */
+    @JSONField(name = "wallet_type")
+    protected String            walletType;
+    
+    /** (必填)订单标题 */
+    @JSONField(name = "biz_content")
+    protected String            bizContent;
+    
+    class BizRequest{
+        
+        /** (必填)商户识别号 */
+        @JSONField(name = "merchant_id")
+        protected String            merchantId;
+        
+        /** (必填)商户订单号 */
+        @JSONField(name = "out_trade_no")
+        protected String            outTradeNo;
+        
+        /** (必填)支付场景 */
+        @JSONField(name = "scene")
+        protected String            scene;
+        
+        /** (必填)支付授权码 */
+        @JSONField(name = "auth_code")
+        protected String            authCode;
+        
+        /** (必填)订单标题 */
+        @JSONField(name = "subject")
+        protected String            subject;
+        
+        public String buildBizContent()
+        {
+             Map<String, String> paraMap = new TreeMap<String, String>();
+             
+             if (StringUtils.isNotBlank(merchantId)) {
+                 paraMap.put("merchant_id", merchantId);
+             }
+             if (StringUtils.isNotBlank(outTradeNo)) {
+                 paraMap.put("out_trade_no", outTradeNo);
+             }
+             if (StringUtils.isNotBlank(scene)) {
+                 paraMap.put("scene", scene);
+             }
+             if (StringUtils.isNotBlank(authCode)) {
+                 paraMap.put("auth_code", authCode);
+             }
+             if (StringUtils.isNotBlank(subject)) {
+                 paraMap.put("subject", subject);
+             }
+             
+             return JSON.toJSONString(paraMap);
+        }
+
+
+        public String getMerchantId() {
+            return merchantId;
+        }
+
+
+        public void setMerchantId(String merchantId) {
+            this.merchantId = merchantId;
+        }
+
+
+        public String getOutTradeNo() {
+            return outTradeNo;
+        }
+
+
+        public void setOutTradeNo(String outTradeNo) {
+            this.outTradeNo = outTradeNo;
+        }
+
+
+        public String getScene() {
+            return scene;
+        }
+
+
+        public void setScene(String scene) {
+            this.scene = scene;
+        }
+
+
+        public String getAuthCode() {
+            return authCode;
+        }
+
+
+        public void setAuthCode(String authCode) {
+            this.authCode = authCode;
+        }
+
+
+        public String getSubject() {
+            return subject;
+        }
+
+
+        public void setSubject(String subject) {
+            this.subject = subject;
+        }
+        
+    }
+    
+    /**
+     * 将不为空的参数放入TreeMap<br>
+     */
+    public Map<String, String> buildParaMap() {
+        Map<String, String> paraMap = new TreeMap<String, String>();
+
+        if (StringUtils.isNotBlank(acquirerId)) {
+            paraMap.put("acquirer_id", acquirerId);
+        }
+        if (StringUtils.isNotBlank(appId)) {
+            paraMap.put("app_id", appId);
+        }
+        if (StringUtils.isNotBlank(method)) {
+            paraMap.put("method", method);
+        }
+        if (StringUtils.isNotBlank(charset)) {
+            paraMap.put("charset", charset);
+        }
+        if (StringUtils.isNotBlank(signType)) {
+            paraMap.put("sign_type", signType);
+        }
+        if (StringUtils.isNotBlank(sign)) {
+            paraMap.put("sign", sign);
+        }
+        if (StringUtils.isNotBlank(timestamp)) {
+            paraMap.put("timestamp", timestamp);
+        }
+        if (StringUtils.isNotBlank(version)) {
+            paraMap.put("version", version);
+        }
+        if (StringUtils.isNotBlank(notifyUrl)) {
+            paraMap.put("notify_url", notifyUrl);
+        }
+        if (StringUtils.isNotBlank(appAuthToken)) {
+            paraMap.put("app_auth_token", appAuthToken);
+        }
+        if (StringUtils.isNotBlank(walletType)) {
+            paraMap.put("wallet_type", walletType);
+        }
+
+        return paraMap;
+    }
+    
+    /**
+     * 将Map中的参数转换成NamValuePair对，并封装成List
+     */
+    public List<NameValuePair> buildPostParaList(Map<String, String> paraMap) {
+
+        List<NameValuePair> pairList = new ArrayList<NameValuePair>(paraMap.size());
+
+        for (String key : paraMap.keySet()) {
+            NameValuePair nvPair = new NameValuePair(key, paraMap.get(key));
+            pairList.add(nvPair);
+        }
+
+        return pairList;
+    }
+
+    public String getAcquirerId() {
+        return acquirerId;
+    }
+
+    public void setAcquirerId(String acquirerId) {
+        this.acquirerId = acquirerId;
+    }
+
+    public String getAppId() {
+        return appId;
+    }
+
+    public void setAppId(String appId) {
+        this.appId = appId;
+    }
+
+    public String getMethod() {
+        return method;
+    }
+
+    public void setMethod(String method) {
+        this.method = method;
+    }
+
+    public String getFormat() {
+        return format;
+    }
+
+    public void setFormat(String format) {
+        this.format = format;
+    }
+
+    public String getCharset() {
+        return charset;
+    }
+
+    public void setCharset(String charset) {
+        this.charset = charset;
+    }
+
+    public String getSignType() {
+        return signType;
+    }
+
+    public void setSignType(String signType) {
+        this.signType = signType;
+    }
+
+    public String getSign() {
+        return sign;
+    }
+
+    public void setSign(String sign) {
+        this.sign = sign;
+    }
+
+    public String getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(String timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    public String getVersion() {
+        return version;
+    }
+
+    public void setVersion(String version) {
+        this.version = version;
+    }
+
+    public String getNotifyUrl() {
+        return notifyUrl;
+    }
+
+    public void setNotifyUrl(String notifyUrl) {
+        this.notifyUrl = notifyUrl;
+    }
+
+    public String getAppAuthToken() {
+        return appAuthToken;
+    }
+
+    public void setAppAuthToken(String appAuthToken) {
+        this.appAuthToken = appAuthToken;
+    }
+
+    public String getWalletType() {
+        return walletType;
+    }
+
+    public void setWalletType(String walletType) {
+        this.walletType = walletType;
+    }
+
+    public String getBizContent() {
+        return bizContent;
+    }
+
+    public void setBizContent(String bizContent) {
+        this.bizContent = bizContent;
+    }
+   
+}
+
---
+++ mPayTradeCore-service/src/test/java/org/tradecore/acquirertest/AcquirerPayTest.java
@@ -0,0 +1,110 @@
+package org.tradecore.acquirertest;
+import java.nio.charset.StandardCharsets;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.annotation.Resource;
+
+import org.apache.commons.httpclient.NameValuePair;
+import org.apache.commons.lang3.StringUtils;
+import org.junit.Assert;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.tradecore.alipay.enums.AlipayBizResultEnum;
+import org.tradecore.alipay.enums.AlipaySceneEnum;
+import org.tradecore.alipay.service.TradeServiceTest;
+import org.tradecore.alipay.trade.constants.ParamConstant;
+import org.tradecore.alipay.trade.request.CancelRequest;
+import org.tradecore.alipay.trade.request.PayRequest;
+import org.tradecore.alipay.trade.request.PrecreateRequest;
+import org.tradecore.alipay.trade.request.QueryRequest;
+import org.tradecore.alipay.trade.request.RefundRequest;
+import org.tradecore.alipay.trade.service.TradeService;
+import org.tradecore.common.util.HttpUtil;
+import org.tradecore.common.util.ImageUtil;
+import org.tradecore.common.util.LogUtil;
+import org.tradecore.service.test.BaseTest;
+
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.serializer.SerializerFeature;
+import com.alipay.api.domain.GoodsDetail;
+import com.alipay.api.internal.util.AlipaySignature;
+import com.alipay.api.response.AlipayTradeCancelResponse;
+import com.alipay.api.response.AlipayTradePayResponse;
+import com.alipay.api.response.AlipayTradePrecreateResponse;
+import com.alipay.api.response.AlipayTradeQueryResponse;
+import com.alipay.api.response.AlipayTradeRefundResponse;
+
+/**
+ * 
+ * @author prd-wfc
+ *
+ */
+public class AcquirerPayTest {
+     /** 日志 */
+    private static final Logger logger = LoggerFactory.getLogger(TradeServiceTest.class);
+    
+    
+    /**
+     * 
+     * @throws Exception
+     */
+    @Test
+    public void testPay() throws Exception {
+
+        //Assert.assertNotNull(tradeService);
+        //组装参数
+        String merchantNotifyUrl="http://127.0.0.1:8088/mPay/trade/pay";
+        AcquirerPayRequest acqPayRequest=new AcquirerPayRequest();
+        acqPayRequest.bizrequest.setMerchantId("34855"); 
+        acqPayRequest.bizrequest.setOutTradeNo("108800100333485511");
+        acqPayRequest.bizrequest.setScene("bar_code");
+        acqPayRequest.bizrequest.setAuthCode("28355684010846005");
+        acqPayRequest.bizrequest.setSubject("结算中心条码交易测试_1471225111111");
+            
+        acqPayRequest.setAcquirerId("10880010033");
+        acqPayRequest.setAppId(new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(System.currentTimeMillis()));
+        acqPayRequest.setMethod("alipay.boss.prod.merch");
+        acqPayRequest.setFormat("JSON");
+        acqPayRequest.setCharset("utf-8");
+        acqPayRequest.setSignType("RSA");
+       
+        
+        acqPayRequest.setTimestamp("2016/8/17 13:07:09");
+        acqPayRequest.setVersion("1.0");
+        acqPayRequest.setNotifyUrl("http://api.test.alipay.net/atinterface/receive_notify.htm");
+        acqPayRequest.setAppAuthToken(" ");
+        acqPayRequest.setWalletType("alipay");
+        acqPayRequest.setBizContent(acqPayRequest.bizrequest.buildBizContent());
+        
+        Map<String, String> map=acqPayRequest.buildParaMap();
+
+        //签名
+        String sign = null;
+        try {
+            sign = AlipaySignature
+                .rsaSign(
+                    map,
+                    "MIICdQIBADANBgkqhkiG9w0BAQEFAASCAl8wggJbAgEAAoGBALcfL22Dcf+R1WC7fpIlb6cYI0oPBtg2iv9K71sAMS5DShA6LavWo0RTqrsCNsSxGuexHwmWYWcPH9469fKTdEtAnACi/Hr2hQcw7vEehqnjYir8eQljqJpjSd7ZhXdDrfLsX4J2CbUEjkrMM0Q7rx/0cuI7b0yMWBRVPtnwALBvAgMBAAECgYAGtivk1aZ9+XhanUScUqbu9uGEO1zC2+zoQnTXXwBuc6TpR1iZLbq6LF7bj882Ek+sIj/C+DIFtvYyDPMquuDOOUvXm03HRWp20xB8X0fxJzQbS1MOvgxwFW4zDFr39UDrCue/nGVC/qrsWjX6IGYPYvAs2sB+jc3DTJpDJtWmQQJBAPMBZdky7FJ/gr+0KhEJdffWWjfkcdOLka6GetbZEiVWOplTdoIv4bjJFja8dRAQU+BeRmvMpvBnmFjH2RrH6nECQQDA6gP3Cri334zOPS6r1IzlqBy2QvI/11Fn/zyEe8MYtQz3Tkh5MX9R9ejK7lqIv9v1bsusG5SGYHyWMmSvnvjfAkBJ747xetD0eN9rPIHgFSTTd2CTyOnpF3oHw9r0K6+dtJK3u/E+wxrGgkhD9ysW7CDZD1YVznqsgpiTypp/z3vBAkA7deXS91MIGbdkuibwf4sOHkr7Qpc4Zj2JOHqGuz7fFq7wawibkk4UDR+7rMvq6nf5pjTQz49v+71q7g1qtC0xAkBr/ZBpOoO5kc4MZ/DR4TOeFXqaWCmDHsNxmtYPBtnTcuFLQCwH10Ab6Yo2owndvpa1PhdJ7b/h9z1Eom1jy0ba",
+                    StandardCharsets.UTF_8.displayName());
+        } catch (Exception e) {
+            throw new RuntimeException("加签发生异常");
+        }
+        acqPayRequest.setSign(sign);      
+        List<NameValuePair> paraList=acqPayRequest.buildPostParaList(map);
+        
+        String response = HttpUtil.httpClientPost(merchantNotifyUrl, paraList);
+        LogUtil.info(logger, "完成发送扫码支付到中心,response={0}", response);
+        Assert.assertTrue(StringUtils.equals(response, ParamConstant.NOTIFY_SUCCESS));
+        //Assert.assertTrue(ret.getCode().equals(AlipayBizResultEnum.SUCCESS.getCode()));
+
+    }
+    
+   
+
+
+}
